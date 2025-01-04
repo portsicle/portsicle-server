32a534076d4919f2188ff616ea4264873a2f458a
Amit Suthar <sutharamit707@gmail.com>
2025-01-05 00:03:23 +0530

    a bit explanation

--- server/handleGET.go
+++ server/handleGET.go
@@ -27,9 +27,19 @@ func HandleGET(w http.ResponseWriter, r *http.Request) {
 
 	// try to get session from referer
 	referer := r.Header.Get("Referer")
-
 	if referer != "" {
 		if refererUrl, err := url.Parse(referer); err == nil {
+
+			/*
+				this if block will handle the case when the sessionId would be in referer header instead of path.
+				like if the browser wants to fetch a resource like a css or a script file.
+				eg: if the requested url by browser is : http://localhost:8081/assets/index-t7wc3-JJ.js, then:
+
+				referer Header: http://localhost:8081/6a205f60-8221-4c36-a63b-d71768b0216c  -> this header will contain our sessionId.
+				referer path:  	6a205f60-8221-4c36-a63b-d71768b0216c  -> fetched from Referer Header.
+				session id:  		6a205f60-8221-4c36-a63b-d71768b0216c  -> session id is set to referer path.
+				request path:  	/assets/index-t7wc3-JJ.js            	-> request path is now the resource we want to request!
+			*/
 
 			refererPath := refererUrl.Path[1:]
 			refererParts := strings.SplitN(refererPath, "/", 2)
@@ -52,6 +62,18 @@ func HandleGET(w http.ResponseWriter, r *http.Request) {
 
 	// If no session-id in referer, fallback to path
 	if sessionId == "" {
+
+		/*
+			this if block will handle the case when the sessionId will be available in the path itself.
+			like the browser is requesting just the base HTML file or something served on base url '/'
+			eg: if the requested url by browser is : http://localhost:8081/6a205f60-8221-4c36-a63b-d71768b0216c, then:
+
+			path: 0fcafcb0-630b-40ab-afa3-adfbb544c7a5 			 -> path contains the sessionId itself
+			referer Header: "" 												 			 -> no referer header
+			session id: 0fcafcb0-630b-40ab-afa3-adfbb544c7a5 -> session id directly from the path
+			request path: /																	 -> request path is now the base path!
+		*/
+
 		if !strings.Contains(path, "/") {
 			sessionId = path
 			requestPath = "/"
@@ -70,6 +92,37 @@ func HandleGET(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	body, _ := io.ReadAll(r.Body)
+	message := Message{
+		Method:  r.Method,
+		Path:    requestPath,
+		Headers: r.Header,
+		Body:    string(body),
+	}
+
+	/*
+		a normal message will look like:
+		{
+			Method:  GET
+			Path: 	 /
+			Headers: map[
+				Accept:[text/html,application/xhtml+xml,application/xml;q=0.9;q=0.8]
+				Accept-Encoding:[gzip, deflate, br, zstd]
+				Accept-Language:[en-US,en;q=0.5]
+				Connection:[keep-alive]
+				User-Agent:[Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0]
+				]
+			Body:    <nil>
+			}
+	*/
+
+	// json encoding of the raw message
+	messageBytes, err := json.Marshal(message)
+	if err != nil {
+		http.Error(w, "Error crafting message", http.StatusInternalServerError)
+		return
+	}
+
 	mu.Lock()
 	conn, exists := clients[sessionId]
 	responseChan, responseExists := responses[sessionId]
@@ -80,20 +133,7 @@ func HandleGET(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	body, _ := io.ReadAll(r.Body)
-	message := Message{
-		Method:  r.Method,
-		Path:    requestPath,
-		Headers: r.Header,
-		Body:    string(body),
-	}
-
-	messageBytes, err := json.Marshal(message)
-	if err != nil {
-		http.Error(w, "Error crafting message", http.StatusInternalServerError)
-		return
-	}
-
+	// send this request message to the client
 	err = conn.WriteMessage(websocket.TextMessage, messageBytes)
 	if err != nil {
 		http.Error(w, "Error sending message to WebSocket", http.StatusInternalServerError)
@@ -101,13 +141,34 @@ func HandleGET(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Waiting for response msg from client
+	/*
+		After routing this GET message to client via socket, we will now read from the responses channel for this session.
+		We expect that the client will now interact with the local server and reply with a response.
+		So we	wait for response message from client untill the channel is closed.
+	*/
+
 	select {
 	case response, ok := <-responseChan:
 		if !ok {
 			http.Error(w, "Connection closed", http.StatusServiceUnavailable)
 			return
 		}
+
+		/*
+			A respose will look like:
+			{
+				Method:  GET
+				Path:    /assets/index-DAWgUG2K.css
+				Headers: map[
+					Accept:[text/css]
+					Accept-Encoding:[gzip, deflate, br, zstd]
+					Connection:[keep-alive]
+					Referer:[http://localhost:8081/114f1771d-426f-9f4c-c14958ac] -> A referer header which indicates that its a response to resource request
+					User-Agent:[Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0]
+				]
+				Body:   0xc00011e060
+			}
+		*/
 
 		if response == nil || response.Response == nil {
 			http.Error(w, "Invalid response received", http.StatusInternalServerError)
@@ -120,8 +181,8 @@ func HandleGET(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 
-		w.WriteHeader(response.Response.StatusCode) // Write header
-		fmt.Fprint(w, response.Response.Body)       // Write body
+		w.WriteHeader(response.Response.StatusCode) // Write Status code
+		fmt.Fprint(w, response.Response.Body)       // Write body -> this is the actual response received for a request
 
 	case <-r.Context().Done():
 		http.Error(w, "Request timeout", http.StatusGatewayTimeout)
--- server/socket.go
+++ server/socket.go
@@ -24,8 +24,9 @@ func HandleSocket(w http.ResponseWriter, r *http.Request) {
 	responses[sessionId] = make(chan *Message)
 	mu.Unlock()
 
-	log.Printf("New WebSocket connection established with session ID: %s", sessionId)
+	log.Printf("New client connection established with session ID: %s", sessionId)
 
+	// the very first message client will receive from server is the sessionId
 	err = conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%s", sessionId)))
 	if err != nil {
 		log.Println("Error sending session ID:", err)
@@ -33,6 +34,13 @@ func HandleSocket(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for {
+
+		/*
+			Infinitely read from this client connection untill error occurs.
+			on error: close the responses channel, delete everything related to this session and close the connection.
+			on receiving a response: Decode the json & write the raw response into the responses channel so the handleGET function can read from it for this channel.
+		*/
+
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
@@ -49,13 +57,14 @@ func HandleSocket(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
-		// response from client
+		// decoding response from client
 		var message Message
 		if err := json.Unmarshal(msg, &message); err != nil {
 			log.Printf("Error unmarshaling message: %v", err)
 			continue
 		}
 
+		// if there's a non-nil message, write it into the responses channel
 		if message.Response != nil {
 			mu.Lock()
 			if ch, exists := responses[sessionId]; exists {
