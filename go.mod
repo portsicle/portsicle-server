4a792aeeee5f985be7e979fd272f2cc5d0f1a7cd
Amit Suthar <sutharamit707@gmail.com>
2025-02-23 15:43:27 +0530

    lz4 + short session ids

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.23.4
 require (
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
-	github.com/shamaton/msgpack/v2 v2.2.2
 	github.com/joho/godotenv v1.5.1
+	github.com/pierrec/lz4 v2.6.1+incompatible
+	github.com/shamaton/msgpack/v2 v2.2.2
 )
+
+require github.com/frankban/quicktest v1.14.6 // indirect
--- server/socket.go
+++ server/socket.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"fmt"
+	"encoding/base64"
 	"log"
 	"net/http"
 
@@ -18,17 +18,18 @@ func HandleSocket(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sessionId := uuid.New().String()
+	b64SessionId := hashSessionId(sessionId, 12)
 	clientConn := &clientConnection{conn: conn}
 
 	mu.Lock()
-	clients[sessionId] = clientConn
-	responses[sessionId] = make(chan *Message)
+	clients[b64SessionId] = clientConn
+	responses[b64SessionId] = make(chan *Message)
 	mu.Unlock()
 
-	log.Printf("New client connection established with session ID: %s", sessionId)
+	log.Printf("New client connection established with session ID: %s", b64SessionId)
 
 	// the very first message client will receive from server is the sessionId
-	err = clientConn.writeMessage(websocket.TextMessage, []byte(fmt.Sprintf("%s", sessionId)))
+	err = clientConn.writeMessage(websocket.TextMessage, []byte(b64SessionId))
 	if err != nil {
 		log.Println("Error sending session ID:", err)
 		return
@@ -39,20 +40,23 @@ func HandleSocket(w http.ResponseWriter, r *http.Request) {
 		/*
 			Infinitely read from this client connection untill error occurs.
 			on error: close the responses channel, delete everything related to this session and close the connection.
-			on receiving a response: Decode the json & write the raw response into the responses channel so the handleGET function can read from it for this channel.
+			on receiving a response: Decode the msgpack & write the raw response into the responses channel so the handleGET function can read from it for this channel.
 		*/
 
 		_, msg, err := conn.ReadMessage()
+		log.Print("received compressed body: ", len(msg), " bytes")
+		msg = lz4decompress(msg)
+		log.Print("after uncompression: ", len(msg), " bytes")
 		if err != nil {
 			if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
-				log.Printf("Client closed connection for session ID: %s", sessionId)
+				log.Printf("Client closed connection for session ID: %s", b64SessionId)
 			} else {
-				log.Printf("Error reading message for session ID %s: %v", sessionId, err)
+				log.Printf("Error reading message for session ID %s: %v", b64SessionId, err)
 			}
 			mu.Lock()
-			delete(clients, sessionId)
-			close(responses[sessionId])
-			delete(responses, sessionId)
+			delete(clients, b64SessionId)
+			close(responses[b64SessionId])
+			delete(responses, b64SessionId)
 			mu.Unlock()
 			conn.Close()
 			break
@@ -68,11 +72,37 @@ func HandleSocket(w http.ResponseWriter, r *http.Request) {
 		// if there's a non-nil message, write it into the responses channel
 		if message.Response != nil {
 			mu.Lock()
-			if ch, exists := responses[sessionId]; exists {
+			if ch, exists := responses[b64SessionId]; exists {
 				ch <- &message
 			}
 			mu.Unlock()
 		}
 
 	}
+}
+
+// hashSessionId tends to generate a collision proof Base64 encoded uuid which is shorter in length.
+func hashSessionId(sessionId string, part int) string {
+	u, err := uuid.Parse(sessionId)
+	if err != nil {
+		log.Print("error parsing uuid: ", err)
+	}
+
+	if part <= 0 {
+		log.Print("Invalid part encountered, falling back to long uuid.")
+		return sessionId
+	}
+
+	// URL safe base 64 encoding of last all the 16 bytes
+	b64SessionId := base64.URLEncoding.EncodeToString([]byte(u[:]))
+	b64 := b64SessionId[:part]
+
+	// adding hyphen at  part/2 length.
+	mid := part / 2
+	if mid > 0 {
+		b64 = b64[:mid] + "-" + b64[mid:]
+	}
+
+	// this returned string is a shorter version which would look like: VucWqW-mjT2yJ
+	return b64
 }
